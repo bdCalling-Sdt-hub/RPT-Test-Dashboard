b70412f177040a83094d9049e01b00e90d72f593
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2024-04-25 10:51:57 +0600

    updat ip

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     host: "192.168.10.46",
-//     port: "8000",
-//   },
-// });
+// })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "45.55.201.156",
+    port: "8000",
+  },
+});

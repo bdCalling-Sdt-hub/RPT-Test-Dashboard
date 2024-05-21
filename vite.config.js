e1b6753f855bda309511fb5a1ff6459c050989bc
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2024-05-21 09:37:56 +0600

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
 // export default defineConfig({
 //   plugins: [react()],
-//   server: {
-//     host: "45.55.201.156",
-//     port: "8000",
-//   },
-// });
+// })
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "45.55.36.124",
+    port: "8000",
+  },
+});
